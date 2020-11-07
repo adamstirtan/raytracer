b97d8bdb5d479c22951b0c5674852258cd561ba7
Adam Stirtan <adamstirtan@gmail.com>
2020-11-07 09:42:00 -0500

    Cleanup scaffolding code

---
+++ RayTracer.Core/Ray.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace RayTracer.Core
+{
+    public class Ray
+    {
+        private Vector3 _origin;
+        private Vector3 _destination;
+
+        public Ray()
+            : this(Vector3.Zero, Vector3.Zero)
+        { }
+
+        public Ray(Vector3 origin, Vector3 destination)
+        {
+            _origin = origin;
+            _destination = destination;
+        }
+    }
+}
--- RayTracer.Server/Controllers/WeatherForecastController.cs
+++ RayTracer.Server/Controllers/WeatherForecastController.cs
@@ -1,40 +1,40 @@
-﻿using RayTracer.Core.Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿//using RayTracer.Core.Shared;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Logging;
 
-namespace RayTracer.Core.Server.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+//namespace RayTracer.Core.Server.Controllers
+//{
+//    [ApiController]
+//    [Route("[controller]")]
+//    public class WeatherForecastController : ControllerBase
+//    {
+//        private static readonly string[] Summaries = new[]
+//        {
+//            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//        };
 
-        private readonly ILogger<WeatherForecastController> logger;
+//        private readonly ILogger<WeatherForecastController> logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            this.logger = logger;
-        }
+//        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+//        {
+//            this.logger = logger;
+//        }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-    }
-}
+//        [HttpGet]
+//        public IEnumerable<WeatherForecast> Get()
+//        {
+//            var rng = new Random();
+//            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+//            {
+//                Date = DateTime.Now.AddDays(index),
+//                TemperatureC = rng.Next(-20, 55),
+//                Summary = Summaries[rng.Next(Summaries.Length)]
+//            })
+//            .ToArray();
+//        }
+//    }
+//}
