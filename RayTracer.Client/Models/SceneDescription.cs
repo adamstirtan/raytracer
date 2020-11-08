8a5e0db8d5decc602797f60ce5f0640f405cbc8f
Adam Stirtan <adamstirtan@gmail.com>
2020-11-08 16:33:36 -0500

    Building the client side design

---
+++ RayTracer.Client/Models/SceneDescription.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RayTracer.Client.Models
+{
+    public class SceneDescription
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Slug { get; set; }
+
+        [Required]
+        public string Json { get; set; }
+    }
+}
