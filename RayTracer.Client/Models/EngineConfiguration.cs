3b5327acd4057feffbada45750836d9f0402b2ac
Adam Stirtan <adamstirtan@gmail.com>
2020-11-10 05:54:17 -0500

    Cleaned up UI

---
+++ RayTracer.Client/Models/EngineConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RayTracer.Client.Models
+{
+    public class EngineConfiguration
+    {
+        [Required]
+        public string Json { get; set; }
+    }
+}
