73437e05f574c88346ca02f74215219012f79a63
Adam Stirtan <adamstirtan@gmail.com>
2026-03-24 11:42:06 -0400

    Tests: construct Material with color

--- RayTracer.Core.Tests/TorusTests.cs
+++ RayTracer.Core.Tests/TorusTests.cs
@@ -12,14 +12,14 @@ public class TorusTests
     [TestMethod]
     public void Torus_PrimitiveType_IsTorus()
     {
-        var torus = new Torus(new Material(), null, 2.0f, 0.5f);
+        var torus = new Torus(new Material(new System.Numerics.Vector3(0.8f,0.3f,0.2f)), null, 2.0f, 0.5f);
         Assert.AreEqual(PrimitiveType.Torus, torus.GetPrimitiveType());
     }
 
     [TestMethod]
     public void Torus_RayMissesWhenFar()
     {
-        var torus = new Torus(new Material(), null, 2.0f, 0.5f);
+        var torus = new Torus(new Material(new System.Numerics.Vector3(0.8f,0.3f,0.2f)), null, 2.0f, 0.5f);
         var ray = new Ray(new Vector3(100,100,100), Vector3.Normalize(new Vector3(1,0,0)));
         float dist = float.MaxValue;
         var res = torus.Intersects(ray, ref dist);
@@ -29,7 +29,7 @@ public class TorusTests
     [TestMethod]
     public void Torus_RayHitsWhenAimed()
     {
-        var torus = new Torus(new Material(), null, 2.0f, 0.5f);
+        var torus = new Torus(new Material(new System.Numerics.Vector3(0.8f,0.3f,0.2f)), null, 2.0f, 0.5f);
         // place ray pointing at torus center from front
         var ray = new Ray(new Vector3(0,0, -10), Vector3.Normalize(new Vector3(0,0,1)));
         float dist = 100f;
