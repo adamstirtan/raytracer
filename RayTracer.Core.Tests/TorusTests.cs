5b6e05b0b07073fcd7a12653d80572d5cb139d92
Adam Stirtan <adamstirtan@gmail.com>
2026-03-24 11:36:13 -0400

    Tests: convert TorusTests to MSTest

--- RayTracer.Core.Tests/TorusTests.cs
+++ RayTracer.Core.Tests/TorusTests.cs
@@ -1,40 +1,40 @@
 using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RayTracer.Core.Materials;
 using RayTracer.Core.Primitives;
 using RayTracer.Core.Math;
-using Xunit;
 
-namespace RayTracer.Core.Tests
+namespace RayTracer.Core.Tests;
+
+[TestClass]
+public class TorusTests
 {
-    public class TorusTests
+    [TestMethod]
+    public void Torus_PrimitiveType_IsTorus()
     {
-        [Fact]
-        public void Torus_PrimitiveType_IsTorus()
-        {
-            var torus = new Torus(new Material(), null, 2.0f, 0.5f);
-            Assert.Equal(PrimitiveType.Torus, torus.GetPrimitiveType());
-        }
+        var torus = new Torus(new Material(), null, 2.0f, 0.5f);
+        Assert.AreEqual(PrimitiveType.Torus, torus.GetPrimitiveType());
+    }
 
-        [Fact]
-        public void Torus_RayMissesWhenFar()
-        {
-            var torus = new Torus(new Material(), null, 2.0f, 0.5f);
-            var ray = new Ray(new Vector3(100,100,100), Vector3.Normalize(new Vector3(1,0,0)));
-            float dist = float.MaxValue;
-            var res = torus.Intersects(ray, ref dist);
-            Assert.Equal(RayIntersection.Miss, res);
-        }
+    [TestMethod]
+    public void Torus_RayMissesWhenFar()
+    {
+        var torus = new Torus(new Material(), null, 2.0f, 0.5f);
+        var ray = new Ray(new Vector3(100,100,100), Vector3.Normalize(new Vector3(1,0,0)));
+        float dist = float.MaxValue;
+        var res = torus.Intersects(ray, ref dist);
+        Assert.AreEqual(RayIntersection.Miss, res);
+    }
 
-        [Fact]
-        public void Torus_RayHitsWhenAimed()
-        {
-            var torus = new Torus(new Material(), null, 2.0f, 0.5f);
-            // place ray pointing at torus center from front
-            var ray = new Ray(new Vector3(0,0, -10), Vector3.Normalize(new Vector3(0,0,1)));
-            float dist = 100f;
-            var res = torus.Intersects(ray, ref dist);
-            Assert.Equal(RayIntersection.Hit, res);
-            Assert.True(dist > 0 && dist < 100f);
-        }
+    [TestMethod]
+    public void Torus_RayHitsWhenAimed()
+    {
+        var torus = new Torus(new Material(), null, 2.0f, 0.5f);
+        // place ray pointing at torus center from front
+        var ray = new Ray(new Vector3(0,0, -10), Vector3.Normalize(new Vector3(0,0,1)));
+        float dist = 100f;
+        var res = torus.Intersects(ray, ref dist);
+        Assert.AreEqual(RayIntersection.Hit, res);
+        Assert.IsTrue(dist > 0 && dist < 100f);
     }
 }
