ba2d1dd94d171ddcb14805e9b8975b314dc9419b
Adam Stirtan <adamstirtan@gmail.com>
2026-03-23 21:57:33 -0400

    Add Box, Cylinder, Disk primitives and scenes

--- RayTracer.Core/Scenes/BoxScene.cs
+++ RayTracer.Core/Scenes/BoxScene.cs
@@ -8,9 +8,9 @@ public class BoxScene : Scene
 {
     public BoxScene()
     {
-        AddObject(new RayTracer.Core.Primitives.Plane(new Vector3(0,1,0), 1000, new Material(new Vector3(0.9f,0.9f,0.9f), diffuse:0.9f), null));
+        AddObject(new RayTracer.Core.Primitives.Plane(new Vector3(0,1,0), 1000, new Material(new Vector3(0.9f,0.9f,0.9f), 0.9f, 0f, 0.1f), null));
 
-        var box = new Box(new Vector3(-1,0,8), new Vector3(1,2,10), new Material(new Vector3(0.7f,0.2f,0.2f), diffuse:0.8f, reflection:0.2f), null);
+        var box = new Box(new Vector3(-1,0,8), new Vector3(1,2,10), new Material(new Vector3(0.7f,0.2f,0.2f), 0.8f, 0.2f, 0.3f), null);
         AddObject(box);
 
         AddLight(new Light(new Vector3(0, 5, -5), float.MinValue, new Material(new Vector3(0.9f,0.9f,0.9f))));
--- RayTracer.Core/Scenes/CylinderScene.cs
+++ RayTracer.Core/Scenes/CylinderScene.cs
@@ -8,9 +8,9 @@ public class CylinderScene : Scene
 {
     public CylinderScene()
     {
-        AddObject(new RayTracer.Core.Primitives.Plane(new Vector3(0,1,0), 1000, new Material(new Vector3(0.9f,0.9f,0.9f), diffuse:0.9f), null));
+        AddObject(new RayTracer.Core.Primitives.Plane(new Vector3(0,1,0), 1000, new Material(new Vector3(0.9f,0.9f,0.9f), 0.9f, 0f, 0.1f), null));
 
-        var cyl = new Cylinder(new Vector3(0,1,8), 1.0f, 2.0f, new Material(new Vector3(0.2f,0.4f,0.8f), diffuse:0.8f, reflection:0.3f), null);
+        var cyl = new Cylinder(new Vector3(0,1,8), 1.0f, 2.0f, new Material(new Vector3(0.2f,0.4f,0.8f), 0.8f, 0.3f, 0.4f), null);
         AddObject(cyl);
 
         AddLight(new Light(new Vector3(0, 5, -5), float.MinValue, new Material(new Vector3(0.9f,0.9f,0.9f))));
